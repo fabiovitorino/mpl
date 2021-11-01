7d81099a393065df5c9c0e70bd563f14da577015
fabivitorino <fabiovitorino1@gmail.comm>
2021-10-31 21:09:51 -0300

    comit get

--- pages/api/callback.js
+++ pages/api/callback.js
@@ -1,9 +1,17 @@
-function callback(request, response) {
-    const dynamicDate = new Date();
+async function callback(request, response) {
+   const clienteResponse = await fetch('http://csw.grupompl.com.br:7256/api/cadastrosgerais/v10/cliente/85188', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'empresa': 1,
+            'Authorization': 'eyJhbGciOiJFUzI1NiJ9.eyJzdWIiOiJtYXJjdXMuYW50b25pbyIsImlzcyI6ImNzd1NlcnZlciIsImlhdCI6MTYzNTcxNTI3MiwiYXVkIjoiY3N3VmlldyIsImNzd1Rva2VuIjoiM3RPb25xS2lGZjNtcWFPRVB4azlJdyIsImRiTmFtZVNwYWNlIjoic2lzdGVtYXMifQ.ZC_NZyXxeylFJIDPXBZpCt-4Do-PDsTNvqjudcksWfe6Tcyn7mI_HxSaURdpvcAgVdkYgJy9YftaBWjNnlKTWg'
+    }
+      })
+      const clienteResponseJson = await clienteResponse.json();
 
-    response.json({
-        date: dynamicDate.toGMTString()
-    })
+      console.log(clienteResponseJson);
+
+      response.json(clienteResponseJson);
 }
 
 export default callback;
