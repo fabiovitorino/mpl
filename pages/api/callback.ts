50658a004fe7ec2ed47ec17ae609d305d203fc37
fabivitorino <fabiovitorino1@gmail.comm>
2022-01-03 10:36:55 -0300

    calback empresa 4

--- pages/api/callback.ts
+++ pages/api/callback.ts
@@ -2,10 +2,15 @@ import {NextApiRequest, NextApiResponse} from 'next';
 
 async function callback(req:NextApiRequest, res:NextApiResponse) {
   try {
-    const apiSecret = "eyJhbGciOiJFUzI1NiJ9.eyJzdWIiOiJldXplYmlvLmFsdmVzIiwiaXNzIjoiY3N3U2VydmVyIiwiaWF0IjoxNjE4MzIwOTYyLCJhdWQiOiJjc3dWaWV3IiwiY3N3VG9rZW4iOiI1eHN2VVJhdGF4MDRjYjcxVmtLVTVRIiwiZGJOYW1lU3BhY2UiOiJzaXN0ZW1hcyJ9.DM521Keo5VokCAt1Xwqcnzx_e2DH_Sozp6gQYfcTkychN4fstu2oM8KwSzJIJFGIgRByd7Wryq1SGI4fslFAuQ"
-    
+        
     const {method, query, body} = req;
     
+    const apiSecret1 = "eyJhbGciOiJFUzI1NiJ9.eyJzdWIiOiJldXplYmlvLmFsdmVzIiwiaXNzIjoiY3N3U2VydmVyIiwiaWF0IjoxNjE4MzIwOTYyLCJhdWQiOiJjc3dWaWV3IiwiY3N3VG9rZW4iOiI1eHN2VVJhdGF4MDRjYjcxVmtLVTVRIiwiZGJOYW1lU3BhY2UiOiJzaXN0ZW1hcyJ9.DM521Keo5VokCAt1Xwqcnzx_e2DH_Sozp6gQYfcTkychN4fstu2oM8KwSzJIJFGIgRByd7Wryq1SGI4fslFAuQ";
+    const apiSecret2 = "eyJhbGciOiJFUzI1NiJ9.eyJzdWIiOiJhbnltYXJrZXRwciIsImlzcyI6ImNzd1NlcnZlciIsImlhdCI6MTY0MTIxNTAxOCwiYXVkIjoiY3N3VmlldyIsImNzd1Rva2VuIjoiSmNMb0pRR3dHWHFNT09nZmRjamlPZyIsImRiTmFtZVNwYWNlIjoic2lzdGVtYXMifQ.ph_LSzwGEmYzP9IZY-dbxscf8XYbAmBuFKbXBPoxpUNGSeYbRHn1H_vjoEG-unsUy5TrO_kcVh7EuY9ebKWufw";
+    
+    let apiSecret=apiSecret1;
+    if (query.empresa=='4') {let apiSecret=apiSecret2};
+
     const dados ={
       "type": body.type,
       "content":body.content
@@ -17,7 +22,7 @@ async function callback(req:NextApiRequest, res:NextApiResponse) {
       method: 'PUT',
       headers: {
           'Content-Type': 'application/json;charset=utf-8',
-          'empresa': '1',
+          'empresa': query.empresa.toString(),
           'Authorization': apiSecret
       },
       body: JSON.stringify(dados),
